cca6f027358a4485fd80fc51519842a51f45f9be
elhagin <omar.elhagin@live.com>
2017-01-01 23:54:26 +0100

    added animations and scripting

--- Assets/Scripts/boss_script.cs
+++ Assets/Scripts/boss_script.cs
@@ -2,20 +2,31 @@
 using System.Collections;
 
 public class boss_script : MonoBehaviour {
+	Animator anim;
+	bool isBossActive = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isBossActive) {
+			
+		}
 	}
 
 	void OnTriggerExit(Collider c) {
 		if (transform.tag == "entry") {
 			GetComponent<Collider> ().isTrigger = false;
+			isBossActive = true;
 		}
+	}
+
+	IEnumerator NormalAttack(float time) {
+		yield return new WaitForSeconds (time);
+
+		anim.SetTrigger ("Normal Attack");
 	}
 }
