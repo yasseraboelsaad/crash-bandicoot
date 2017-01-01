3db8ba8f072d9a9c56ea28784e15c5a59ba226bf
elhagin <omar.elhagin@live.com>
2017-01-01 23:08:13 +0100

    created boss script

---
+++ Assets/Scripts/boss_script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boss_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider c) {
+		if (transform.tag == "entry") {
+			GetComponent<Collider> ().isTrigger = false;
+		}
+	}
+}
