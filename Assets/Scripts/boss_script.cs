4ae45ac22d31ae280b368e602743c253e83f5e5d
elhagin <omar.elhagin@live.com>
2017-01-02 18:12:23 +0100

    added level 4 prefabs

--- Assets/Scripts/boss_script.cs
+++ Assets/Scripts/boss_script.cs
@@ -8,9 +8,11 @@ public class boss_script : MonoBehaviour {
 	public GameObject boss;
 	public Camera mainCam;
 	public Camera bossCam;
+	public int bossHp;
 
 	// Use this for initialization
 	void Start () {
+		bossHp = 3;
 		isBossActive = false;
 		anim = boss.GetComponent<Animator> ();
 		bossCam.enabled = false;
@@ -20,18 +22,16 @@ public class boss_script : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (isBossActive && anim.GetCurrentAnimatorStateInfo(0).tagHash == idleState) {
-			float rand = Random.Range (0, 3);
+			int rand = Random.Range (0, 3);
 
 			if (rand != 2) {
 				if (!anim.GetBool ("heavyAtk")) {
 					anim.SetTrigger ("normalAtk");
 				}
-//				StartCoroutine (NormalAttack (20));	
 			} else {
 				if (!anim.GetBool ("normalAtk")) {
 					anim.SetTrigger ("heavyAtk");
 				}
-//				StartCoroutine (HeavyAttack (10));
 			}
 		}
 	}
@@ -45,13 +45,9 @@ public class boss_script : MonoBehaviour {
 		}
 	}
 
-	IEnumerator NormalAttack(float time) {
-		yield return new WaitForSeconds (time);
-		anim.SetTrigger ("normalAtk");
-	}
-
-	IEnumerator HeavyAttack(float time) {
-		yield return new WaitForSeconds (time);
-		anim.SetTrigger ("heavyAtk");
+	void OnCollisionExit(Collider c) {
+		if (transform.tag == "Boss" && c.transform.tag == "Player") {
+			
+		}
 	}
 }
