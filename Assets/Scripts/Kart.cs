6fd1f8b9274061d7fa78af6f5e65b765c2d7fb13
Khaled Elsheikh <khaledelsheikh994@gmail.com>
2017-01-04 19:10:17 +0100

    level 3 n1
    added crash and his cart and the fruits , masks , holes and boxes to
    the level. Also, crash and his kart are able to move and jump now
    without any problems

---
+++ Assets/Scripts/Kart.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Kart : MonoBehaviour {
+	public int TurnSpeed = 50;
+	public int WalkSpeed = 20;
+	public int apples;
+	public int masks;
+	public int lives;
+	public Text livesText;
+	public Text masksText;
+	public Text applesText;
+	public Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		apples = 0;
+		masks = 0;
+		lives = 3;
+		livesText.text = "Lives : " + lives.ToString ();
+		masksText.text = "Masks : " + masks.ToString ();
+		applesText.text = "Apples : " + apples.ToString ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float v = Input.GetAxis ("Vertical");
+		transform.Translate (Vector3.forward * WalkSpeed * Time.deltaTime  );
+		float h = Input.GetAxis ("Horizontal");
+		transform.Rotate (new Vector3 (0, 1, 0) * h * Time.deltaTime * TurnSpeed);
+		checkKey ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("apple")) {
+			apples = apples + 1;
+			applesText.text = "Apples : " + apples.ToString ();
+		} else {
+			if(other.gameObject.CompareTag("mask")){
+				masks = masks + 1;
+				masksText.text = "Masks : " + masks.ToString ();
+			} else { 
+				if (other.gameObject.CompareTag ("box")) {
+
+				} else {
+					if (other.gameObject.CompareTag ("hole")) {
+						lives = lives - 1;
+						livesText.text = "Lives : " + lives.ToString ();
+					}
+				}
+			}
+		}
+		other.gameObject.SetActive (false);
+	}
+
+	void checkKey() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rigidBody.AddForce (new Vector3 (0, 10, 0), ForceMode.Impulse);
+		}
+	}
+
+
+}
--- Assets/Scripts/box_script.cs
+++ Assets/Scripts/box_script.cs
@@ -5,17 +5,11 @@ public class box_script : MonoBehaviour {
 
 	GameObject player;
 	Animator anim;
-	crash_script controlscript;
-	public GameObject aku_aku;
-	GameObject AkuAkuMask;
-	public GameObject life;
-	GameObject LifeBox;
-
+	static int atakState = Animator.StringToHash("Spin");
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Crash");
 		anim = player.GetComponent<Animator>();
-		controlscript = player.GetComponent<crash_script>();
 	}
 	
 	// Update is called once per frame
@@ -26,17 +20,6 @@ public class box_script : MonoBehaviour {
 			player.transform.position.x < (this.transform.position+ new Vector3(5,0,0)).x &&
 			player.transform.position.x > (this.transform.position- new Vector3(5,0,0)).x &&
 			currentBaseState.IsName("Spin")){
-			GameObject sound = GameObject.Find ("crateBreak");
-			AudioSource audio = sound.GetComponent<AudioSource>();
-			audio.Play ();
-			int rand = Random.Range(1, 4);
-			if(rand == 1){
-				controlscript.wumpacount=controlscript.wumpacount+10;
-			}else if(rand==2){
-				AkuAkuMask = Instantiate (aku_aku, this.transform.position, Quaternion.identity) as GameObject;
-			}else if(rand == 3){
-				LifeBox = Instantiate (life, this.transform.position, Quaternion.identity) as GameObject;
-			}
 			Destroy (this.gameObject);
 		}
 	}
