18fe42cf01fc4b2e70c4ec4dae7f4387dba17642
elhagin <omar.elhagin@live.com>
2017-01-08 20:26:16 +0100

    fixed win script again

--- Assets/Scripts/win_script.cs
+++ Assets/Scripts/win_script.cs
@@ -6,12 +6,12 @@ using System.Text;
 public class win_script : MonoBehaviour {
 
 	GameObject player;
-	crash_script controlscript;
+//	crash_script controlscript;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Crash");
-		controlscript = player.GetComponent<crash_script>();
+//		controlscript = player.GetComponent<crash_script>();
 	}
 	
 	// Update is called once per frame
@@ -21,22 +21,27 @@ public class win_script : MonoBehaviour {
 
 	void OnTriggerEnter(Collider c){
 		if (player.GetComponent <Collider> () == c) {
-			if (crash_script.lastScene.EndsWith ("4")) {
-				SceneManager.LoadScene ("credits");
+			if (Application.loadedLevelName.Equals ("Level 3")) {
+				SceneManager.LoadScene ("TransitionToLevel4");
 			} 
 			else {
+				if (crash_script.lastScene.EndsWith ("4")) {
+					SceneManager.LoadScene ("credits");
+				} 
+				else {
 //				string newScene = crash_script.lastScene;
-				StringBuilder sb = new StringBuilder (crash_script.lastScene);
-				int sceneNumber = int.Parse (sb.ToString().Substring (sb.Length - 1));
-				int newSceneNumber = sceneNumber + 1;
-				sb.Length--;
-				string newScene = sb.ToString ();
-				newScene += newSceneNumber.ToString ();
-				newScene = newScene.Replace (" ", string.Empty);
-				newScene = "TransitionTo" + newScene;
-				SceneManager.LoadScene (newScene);
+					StringBuilder sb = new StringBuilder (crash_script.lastScene);
+					int sceneNumber = int.Parse (sb.ToString ().Substring (sb.Length - 1));
+					int newSceneNumber = sceneNumber + 1;
+					sb.Length--;
+					string newScene = sb.ToString ();
+					newScene += newSceneNumber.ToString ();
+					newScene = newScene.Replace (" ", string.Empty);
+					newScene = "TransitionTo" + newScene;
+					print (newScene);
+					SceneManager.LoadScene (newScene);
+				}
 			}
-
 		}
 	}
 
