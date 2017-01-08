f88f0ee96ca3aca9c79fa529e2f163b10474c772
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2017-01-08 18:43:41 +0100

    fixed life script

--- Assets/Scripts/life_script.cs
+++ Assets/Scripts/life_script.cs
@@ -5,11 +5,14 @@ public class life_script : MonoBehaviour {
 
 	GameObject player;
 	crash_script controlscript;
+	kart_script controlscript2;
+
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Crash");
 		controlscript = player.GetComponent<crash_script>();
+		controlscript2 = player.GetComponent<kart_script>();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +22,11 @@ public class life_script : MonoBehaviour {
 
 	void OnTriggerEnter(Collider c){
 		if (player.GetComponent <Collider> () == c) {
-			controlscript.hp++;
+			if(Application.loadedLevelName.Equals ("Level 3")){
+				controlscript2.hp++;
+			}else{
+				controlscript.hp++;
+			}
 			Destroy (this.gameObject);
 		}
 	}
